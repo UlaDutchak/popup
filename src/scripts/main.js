a8aa44c5ad6836d36bf1da2c2945c7d8bd33eb3a
Ula Dutchak <ulana.dutchak@gmail.com>
2021-10-15 15:15:00 +0300

    add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
-'use strict';
+const popup = document.getElementById('popup');
+const buttonYes = document.getElementsByClassName('popup__buttons-yes button');
+const buttonNo = document.getElementsByClassName('popup__buttons-no')[0];
+const input = document.getElementById('email');
+const text = document.getElementById('text');
+
+window.onload = function () {
+  if(!localStorage.getItem("popup1")) {
+    setTimeout(function () {
+      popup.style.opacity = "1";
+    }, 3000);
+  } 
+}
+
+buttonNo.onclick = function() {
+  popup.style.display = "none";
+}
+
+buttonYes.onclick = function(event) {
+  if (event.target.buttonYes) {
+    popup.style.display='none';
+    localStorage.setItem('popup1', 'none');
+  }
+}
